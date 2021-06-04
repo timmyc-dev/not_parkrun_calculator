dcb560adabff6d3f2ad28838fce9dcda3c4d9096
Tim Carr <hello@timmyc.dev>
2021-06-04 23:39:28 +0100

    02 Added Enter key behave like tab and checkbox for automatic copy

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # not_parkrun_calculator
+
+Calculator allows you to enter a distance (in miles) and a time, split into hours, minutes and seconds.
+Click the button and be given the time for running a 5k distance
--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 document.addEventListener('DOMContentLoaded', function(){ 
+  document.getElementById('clip').addEventListener('click',()=>copySpec(event))
+  document.getElementById('btCalculate').addEventListener('click', ()=>calculateTime(event))
+  document.addEventListener('keydown', ({key})=>{
+    if (key === "Enter"  && event.target.nodeName === 'INPUT') {
+      console.log(event.target.getAttribute('data-enterkey'));
+      if (event.target.getAttribute('data-enterkey') == 'calculate') {
+        calculateTime()
+      }
+      else {
+        const idNext = event.target.getAttribute('data-enterkey');
+        console.log(`Next ${idNext}`);
+        
+        document.getElementById(idNext).focus();      
+        
+      }
+    }
+  })
   document.getElementById('distance').focus();
   
 }, false);
 
-const copySpec = (event) => {
-  if (document.getElementById('result').innerHTML == 'Waiting for result') calculateTime();
+const enterKey = ({key}) => {
   
-  console.log(` Inner HTML: ${document.getElementById('clipText').value}`);
+}
+
+const copySpec = () => {
+  if (document.getElementById('result').innerHTML == 'Waiting for result') {calculateTime()}
+  
+  const clipText = document.getElementById('clipText');
+
+  if (window.getComputedStyle(clipText).visibility === "hidden") {
+  console.log('It is hidden');
+  clipText.style.visibility = 'visible';
   const copyText = document.querySelector('#clipText');
   copyText.select();
   document.execCommand('copy');
+  clipText.style.visibility = 'hidden';
+  }
+  console.log(` Inner HTML: ${document.getElementById('clipText').value}`);
+
 
 }
 
@@ -28,9 +57,15 @@ function calculateTime(event) {
   let notMinutes;
   let notSeconds;
 
-  let notparkruntime_seconds = (((hours * 3600) + (minutes * 60) + seconds)/distance  * 3.125);
+    // 1 mile = 1609.34 meters
+    // 5km = 5000m = 3.106856 miles
+
+    // notparkruntime_seconds = total 5km time in (whole) seconds
+  let notparkruntime_seconds = (((hours * 3600) + (minutes * 60) + seconds)/distance  * 3.106856);
   notparkruntime_seconds = Math.ceil(notparkruntime_seconds)
-  if (notparkruntime_seconds > 3600) {
+
+    // deconstruct in to hours, minutes and seconds
+  if (notparkruntime_seconds >= 3600) {
       notHours = Math.floor(notparkruntime_seconds/3600);
       notparkruntime_seconds = notparkruntime_seconds - (notHours * 3600);
   } else {
@@ -40,15 +75,21 @@ function calculateTime(event) {
       notMinutes = Math.floor(notparkruntime_seconds/60);
       notparkruntime_seconds = notparkruntime_seconds - (notMinutes * 60);
   } else {
-      notMinutes = "";
+      notMinutes = 0;
   }
   notSeconds = notparkruntime_seconds;
+
+
 
   let resultInnerHtml = "";
   if (notHours !== "") {
       resultInnerHtml = `${notHours}H: `
   }
-  resultInnerHtml = `${resultInnerHtml}${notMinutes}Min: ${notSeconds}Sec:`
+
+  if (notMinutes >0 || notHours !== "") {
+    resultInnerHtml = `${resultInnerHtml}${notMinutes}Min: `  
+  }
+  resultInnerHtml = `${resultInnerHtml}${notSeconds}Sec`
   document.getElementById("result").innerHTML = resultInnerHtml;
 
   // Get data ready for clipboard copy
@@ -58,19 +99,15 @@ function calculateTime(event) {
   notSeconds = "" ? notSeconds = "00" : notSeconds.toString().padStart(2,"0");
   const clipString = `${notHours}:${notMinutes}:${notSeconds}`;
   document.getElementById('clipText').value = clipString;
+  document.getElementById('result').title = clipString;
 
+  console.log(document.querySelector('#copyToClipboard').checked);
   
-// Debugging    
-  // document.getElementById("dbValue0").innerHTML = `${hours} Hours`;
-  // document.getElementById("dbValue1").innerHTML = `${notparkruntime_seconds} Total seconds`;
-  // document.getElementById("dbValue2").innerHTML = notMinutes + " minutes portion for 5k";
-  // document.getElementById("dbValue3").innerHTML = `${notSeconds} seconds portion for 5k`;
-  // document.getElementById("dbValue4").innerHTML = distance + " Distance from input screen";
-// End of debug
-
-
-// alert(notMinutes + "min " + notSeconds + "sec");
-  
+  if (document.querySelector('#copyToClipboard').checked) {
+    console.log('Copying');
+    
+    copySpec()
+  }
   
 }
 
